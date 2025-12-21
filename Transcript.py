9be6fe27d54356015d44af544292f4811e0ecb5e
Poornasai327 <poornasaimamidi327@gmail.com>
2025-12-21 22:01:11 +0530

    Enhance transcript fetching with user options
    Added options for manual, auto-generated, and translated transcripts with error handling for missing transcripts.

--- Transcript.py
+++ Transcript.py
@@ -1,7 +1,30 @@
-from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api import YouTubeTranscriptApi, NoTranscriptFound ,TranscriptsDisabled
 
 video_id = input("Enter the YouTube video ID: ")
-raw_script=YouTubeTranscriptApi().fetch(video_id)
+
+try:
+   transcript_list=YouTubeTranscriptApi().list(video_id)
+   print(transcript_list)
+except TranscriptsDisabled or NoTranscriptFound:
+   print('No Transcript found for the given video ID.')
+   exit()
+
+Required_Script=input('Enter 1 for Manual or 2 for Auto Generated or 3 for Translated: ')
+Required_Language=input('Enter the Required Language code: ')
+
+if Required_Script=='1':
+   try:
+      raw_script=transcript_list.find_manually_created_transcript([Required_Language]).fetch()
+   except NoTranscriptFound:
+      print('No Manual Transcript found for the given language code.')
+      exit()
+
+elif Required_Script=='2':
+   try:
+      raw_script=transcript_list.find_generated_transcript([Required_Language]).fetch()
+   except NoTranscriptFound:
+      print('No Auto Generated Transcript found for the given language code.')
+      exit()
 
 script='\n'
 
@@ -11,8 +34,8 @@ for line in raw_script:
 
 File_Name=input("Enter the File Name: ") + '.txt'
 
-f= open(File_Name,'w')
+f= open(File_Name,'w',encoding='utf-8')
 f.write(script)
 f.close
 
-print('Given Video is Transcripted Succesfully')
+print('Given Video is Transcripted Succesfully to the ' + File_Name + ' in the required language')
