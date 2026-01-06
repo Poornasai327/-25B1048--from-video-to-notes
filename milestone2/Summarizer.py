9edcf49b662000c40ce5f43bbb566577bc94a5c6
Poornasai327 <poornasaimamidi327@gmail.com>
2026-01-06 17:07:19 +0530

    Refactor transcript summarization logic
    Updated chunking logic to use character count instead of word count for better control over chunk sizes. Adjusted input prompts to reflect new chunk size and overlap size requirements.

--- milestone2/Summarizer.py
+++ milestone2/Summarizer.py
@@ -7,20 +7,21 @@ summarizer = pipeline(
     device=-1
 )
 
-def chunk_transcript(transcript,chunk_size,over_lap):
-    words=transcript.split()
-    chunks=[]
-    for  i in range(0,len(words),chunk_size-over_lap):
-        end=i+chunk_size
-        chunk=" ".join(words[i:end])
+def chunk_transcript(transcript, chunk_size, overlap):
+    chunks = []
+    start = 0
 
-        if len(chunk.split())<50:
-            continue
+    while start < len(transcript):
+        end = start + chunk_size
+        chunk = transcript[start:end]
         chunks.append(chunk)
+        start = end - overlap
+
     return chunks
 
+
 def summarize_chunk(chunk):
-    summary=summarizer(chunk,max_length=150,min_length=60,do_sample=False)
+    summary=summarizer(chunk,max_length=130,min_length=60,do_sample=False)
     return summary[0]['summary_text']
 
 def summarize_transcript(transcript,chunk_size,over_lap):
@@ -33,8 +34,8 @@ if __name__=="__main__":
     file_name=input("Enter the transcript file name: ")+".txt"
     with open(file_name,'r') as file:
         transcript=file.read()
-    chunk_size=int(input("Enter chunk size (number of words): "))
-    over_lap=int(input("Enter overlap size (number of words): "))
+    chunk_size=int(input("Enter chunk size (800-1400) (number of characters): "))
+    over_lap=int(input("Enter overlap size (100-200) (number of characters): "))
     summary=summarize_transcript(transcript,chunk_size,over_lap)
 
     new_file_name=input("Enter the output summary file name: ")+".txt"
@@ -43,8 +44,8 @@ if __name__=="__main__":
     
     optional_print=input("Do you want to summarize the summary again? if yes type 1 else type 0: ")
     if optional_print=='1':
-        new_chunk_size=int(input("Enter new chunk size (number of words): "))
-        new_over_lap=int(input("Enter new overlap size (number of words): "))
+        new_chunk_size=int(input("Enter new chunk size (number of characters): "))
+        new_over_lap=int(input("Enter new overlap size (number of characters): "))
         summary_again=summarize_transcript(summary,new_chunk_size,new_over_lap)
         final_file_name=input("Enter the final output summary file name: ")+".txt"
         with open(final_file_name,'w') as file:
